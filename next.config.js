e679c9e3e2d7a59e6a3d4b281fc9105d6855ef30
Jintao <pkuxjt@sina.com>
2024-05-02 17:32:11 +0800

    remove basePath

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
   assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
+  basePath = ``;
 }
 const nextConfig = {
     basePath,
